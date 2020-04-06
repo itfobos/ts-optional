b6e9d5e09dc126baab836b038ab96ccc340e2202
ILYA Turukin <tr.wolfer@gmail.com>
2020-04-06 17:07:45 +0700

    [#9] Fixed TypeDoc(Javadoc analog) in the project.
    Fixed TODOs in 'Optional'.

--- src/optional.ts
+++ src/optional.ts
@@ -3,23 +3,20 @@ import {isNotNullOrUndefined, isNullOrUndefined, requireNonNull} from './object-
 
 /**
  * A container object which may or may not contain a non-null value.
- * If a value is present, {@code isPresent()} will return {@code true} and
- * {@code get()} will return the value.
+ * If a value is present, {@linkcode isPresent} will return <code>true</code> and
+ * {@linkcode get} will return the value.
  *
  * <p>Additional methods that depend on the presence or absence of a contained
- * value are provided, such as {@link orElse}
+ * value are provided, such as {@linkcode orElse}
  * (return a default value if value not present) and
- * {@link ifPresent} (execute a block of code if the value is present).
+ * {@linkcode ifPresent} (execute a block of code if the value is present).
  *
- * <p>This is a <a href="../lang/doc-files/ValueBased.html">value-based</a>
- * class; use of identity-sensitive operations (including reference equality
- * ({@code ==}), identity hash code, or synchronization) on instances of
- * {@code Optional} may have unpredictable results and should be avoided.
- *
+ * <p>This is a <a href="https://docs.oracle.com/javase/11/docs/api/java/lang/doc-files/ValueBased.html">value-based</a>
+ * class.
  */
 export class Optional<T> {
   /**
-   * Common instance for {@code empty()}.
+   * Common instance for {@linkcode empty}.
    */
   private static readonly EMPTY: Optional<any> = new Optional<any>();
 
@@ -29,16 +26,16 @@ export class Optional<T> {
   private readonly value: T | null | undefined;
 
   /**
-   * Returns an empty {@code Optional} instance.  No value is present for this
+   * Returns an empty {@linkcode Optional} instance.  No value is present for this
    * Optional.
+   * <br/>
+   * Though it may be tempting to do so, avoid testing if an object
+   * is empty by comparing with <code>==</code> against instances returned by
+   * {@linkcode empty}. There is no guarantee that it is a singleton.
+   * Instead, use {@linkcode isPresent}.
    *
-   * @apiNote Though it may be tempting to do so, avoid testing if an object
-   * is empty by comparing with {@code ==} against instances returned by
-   * {@code Option.empty()}. There is no guarantee that it is a singleton.
-   * Instead, use {@link #isPresent()}.
-   *
-   * @template <T> Type of the non-existent value
-   * @return an empty {@code Optional}
+   * @template T Type of the non-existent value
+   * @return an empty {@linkcode Optional}
    */
   public static empty<T>(): Optional<T> {
     return Optional.EMPTY as Optional<T>;
@@ -60,11 +57,11 @@ export class Optional<T> {
   }
 
   /**
-   * Returns an {@code Optional} with the specified present non-null value.
+   * Returns an {@linkcode Optional} with the specified present non-null value.
    *
-   * @template <T> the class of the value
+   * @template T the class of the value
    * @param value the value to be present, which must be non-null
-   * @return an {@code Optional} with the value present
+   * @return an {@linkcode Optional} with the value present
    * @throws Error if value is null
    */
   public static of<T>(value: T): Optional<T> {
@@ -72,13 +69,13 @@ export class Optional<T> {
   }
 
   /**
-   * Returns an {@code Optional} describing the specified value, if non-null,
-   * otherwise returns an empty {@code Optional}.
+   * Returns an {@linkcode Optional} describing the specified value, if non-null,
+   * otherwise returns an empty {@linkcode Optional}.
    *
-   * @template <T> the class of the value
+   * @template T the class of the value
    * @param value the possibly-null value to describe
-   * @return an {@code Optional} with a present value if the specified value
-   * is non-null, otherwise an empty {@code Optional}
+   * @return an {@linkcode Optional} with a present value if the specified value
+   * is non-null, otherwise an empty {@linkcode Optional}
    */
   public static ofNullable<T>(value: T | null | undefined): Optional<T> {
     return isNullOrUndefined(value) ? Optional.empty() : Optional.of(value as T);
@@ -89,13 +86,13 @@ export class Optional<T> {
   }
 
   /**
-   * If a value is present in this {@code Optional}, returns the value,
-   * otherwise throws {@code NoSuchElementException}.
+   * If a value is present in this {@linkcode Optional}, returns the value,
+   * otherwise throws {@linkcode Error}.
    *
-   * @return the non-null value held by this {@code Optional}
-   * @throws NoSuchElementException if there is no value present
+   * @return the non-null value held by this {@linkcode Optional}
+   * @throws {@linkcode Error} if there is no value present
    *
-   * @see Optional#isPresent()
+   * @see {@linkcode isPresent}
    */
   public get(): T {
     if (isNullOrUndefined(this.value)) {
@@ -105,15 +102,14 @@ export class Optional<T> {
   }
 
   public isPresent(): boolean {
-    //TODO: Implement via !empty
-    return isNotNullOrUndefined(this.value);
+    return !this.isEmpty();
   }
 
   /**
-   * If a value is  not present, returns {@code true}, otherwise
-   * {@code false}.
+   * If a value is  not present, returns <code>true</code>, otherwise
+   * <code>false</code>.
    *
-   * @return value {@code true} if a value is not present, otherwise {@code false}
+   * @return value </code>true</code> if a value is not present, otherwise </code>false</code>
    */
   public isEmpty(): boolean {
     return isNullOrUndefined(this.value);
@@ -124,8 +120,7 @@ export class Optional<T> {
    * otherwise do nothing.
    *
    * @param consumer block to be executed if a value is present
-   * @throws NullPointerException if value is present and {@code consumer} is
-   * null
+   * @throws Error if value is present and <code>consumer</code> is null
    */
   public ifPresent(consumer: Consumer<T>): void {
     if (isNotNullOrUndefined(this.value)) {
@@ -140,9 +135,9 @@ export class Optional<T> {
    * @param action the action to be performed, if a value is present
    * @param emptyAction the empty-based action to be performed, if no value is
    *        present
-   * @throws NullPointerException if a value is present and the given action
-   *         is {@code null}, or no value is present and the given empty-based
-   *         action is {@code null}.
+   * @throws Error if a value is present and the given action
+   *         is <code>null</code>, or no value is present and the given empty-based
+   *         action is <code>null</code>.
    */
   public ifPresentOrElse(action: Consumer<T>, emptyAction: Callback): void {
     if (isNotNullOrUndefined(this.value)) {
@@ -154,18 +149,18 @@ export class Optional<T> {
 
   /**
    * If a value is present, and the value matches the given predicate,
-   * return an {@code Optional} describing the value, otherwise return an
-   * empty {@code Optional}.
+   * return an {@linkcode Optional} describing the value, otherwise return an
+   * empty {@linkcode Optional}.
    *
    * @param predicate a predicate to apply to the value, if present
-   * @return an {@code Optional} describing the value of this {@code Optional}
+   * @return an {@linkcode Optional} describing the value of this {@linkcode Optional}
    * if a value is present and the value matches the given predicate,
-   * otherwise an empty {@code Optional}
-   * @throws NullPointerException if the predicate is null
+   * otherwise an empty {@linkcode Optional}
+   * @throws Error if the predicate is <code>null</code>
    */
   public filter(predicate: Predicate<T>): Optional<T> {
     requireNonNull(predicate);
-    if (!this.isPresent()) { //TODO:
+    if (this.isEmpty()) {
       return this;
     } else {
       return predicate(this.nonNullValue) ? this : Optional.empty();
@@ -174,36 +169,34 @@ export class Optional<T> {
 
   /**
    * If a value is present, apply the provided mapping function to it,
-   * and if the result is non-null, return an {@code Optional} describing the
-   * result.  Otherwise return an empty {@code Optional}.
+   * and if the result is non-null, return an {@linkcode Optional} describing the
+   * result.  Otherwise return an empty {@linkcode Optional}.
+   * <br/>
+   * This method supports post-processing on optional values, without
+   * the need to explicitly check for a return status.
+   * <br/>
+   * For example, the following code creates Optional around a response,
+   * and then checks is the received request successful.
    *
-   * @apiNote This method supports post-processing on optional values, without
-   * the need to explicitly check for a return status.  For example, the
-   * following code traverses a stream of file names, selects one that has
-   * not yet been processed, and then opens that file, returning an
-   * {@code Optional<FileInputStream>}:
+   * <pre>
+   *    const responseIsSuccessful = Optional.ofNullable(response)
+   *                                         .map(resp => resp.isSuccessful)
+   *                                         .orElse(false);
+   * </pre>
    *
-   * <pre>{@code
-   *     Optional<FileInputStream> fis =
-   *         names.stream().filter(name -> !isProcessedYet(name))
-   *                       .findFirst()
-   *                       .map(name -> new FileInputStream(name));
-   * }</pre>
+   * Here, `Optional.ofNullable(response)` returns an `Optional<any>`, and then
+   * {@linkcode map} returns an `Optional<boolean>`.
    *
-   * Here, {@code findFirst} returns an {@code Optional<String>}, and then
-   * {@code map} returns an {@code Optional<FileInputStream>} for the desired
-   * file if one exists.
-   *
-   * @template <U> The type of the result of the mapping function
+   * @template U The type of the result of the mapping function
    * @param mapper a mapping function to apply to the value, if present
-   * @return an {@code Optional} describing the result of applying a mapping
-   * function to the value of this {@code Optional}, if a value is present,
-   * otherwise an empty {@code Optional}
-   * @throws NullPointerException if the mapping function is null
+   * @return an {@linkcode Optional} describing the result of applying a mapping
+   * function to the value of this {@linkcode Optional}, if a value is present,
+   * otherwise an empty {@linkcode Optional}
+   * @throws Error if the mapping function is null
    */
   public map<U>(mapper: Function<T, U>): Optional<U> {
     requireNonNull(mapper);
-    if (!this.isPresent()) { //TODO: Use is empty
+    if (this.isEmpty()) {
       return Optional.empty();
     } else {
       return Optional.ofNullable(mapper(this.nonNullValue));
@@ -211,25 +204,24 @@ export class Optional<T> {
   }
 
   /**
-   * If a value is present, apply the provided {@code Optional}-bearing
+   * If a value is present, apply the provided {@linkcode Optional}-bearing
    * mapping function to it, return that result, otherwise return an empty
-   * {@code Optional}.  This method is similar to {@link #map(Function)},
-   * but the provided mapper is one whose result is already an {@code Optional},
-   * and if invoked, {@code flatMap} does not wrap it with an additional
-   * {@code Optional}.
+   * {@linkcode Optional}.  This method is similar to {@linkcode map},
+   * but the provided mapper is one whose result is already an {@linkcode Optional},
+   * and if invoked, {@linkcode flatMap} does not wrap it with an additional
+   * {@linkcode Optional}.
    *
-   * @template <U> The type parameter to the {@code Optional} returned by
+   * @template U The type parameter to the {@linkcode Optional} returned by
    * @param mapper a mapping function to apply to the value, if present
    *           the mapping function
-   * @return the result of applying an {@code Optional}-bearing mapping
-   * function to the value of this {@code Optional}, if a value is present,
-   * otherwise an empty {@code Optional}
-   * @throws NullPointerException if the mapping function is null or returns
-   * a null result
+   * @return the result of applying an {@linkcode Optional}-bearing mapping
+   * function to the value of this {@linkcode Optional}, if a value is present,
+   * otherwise an empty {@linkcode Optional}
+   * @throws Error if the mapping function is null or returns a null result
    */
   public flatMap<U>(mapper: Function<T, Optional<U>>): Optional<U> {
     requireNonNull(mapper);
-    if (!this.isPresent()) {
+    if (this.isEmpty()) {
       return Optional.empty();
     } else {
       return requireNonNull(mapper(this.nonNullValue));
@@ -237,50 +229,43 @@ export class Optional<T> {
   }
 
   /**
-   * Return the value if present, otherwise return {@code other}.
+   * Return the value if present, otherwise return <code>other</code>.
    *
    * @param other the value to be returned if there is no value present, may
    * be null
-   * @return the value, if present, otherwise {@code other}
+   * @return the value, if present, otherwise <code>other</code>
    */
   public orElse(other: T): T {
     return !isNullOrUndefined(this.value) ? this.nonNullValue : other;
   }
 
   /**
-   * Return the value if present, otherwise invoke {@code other} and return
+   * Return the value if present, otherwise invoke <code>other</code> and return
    * the result of that invocation.
    *
-   * @param other a {@code Supplier} whose result is returned if no value
+   * @param other a {@linkcode Supplier} whose result is returned if no value
    * is present
-   * @return the value if present otherwise the result of {@code other.get()}
-   * @throws NullPointerException if value is not present and {@code other} is
-   * null
+   * @return the value if present otherwise the result of <code>other()</code>
+   * @throws Error if value is not present and <code>other</code> is null
    */
   public orElseGet<U>(other: Producer<U>): T | U {
     return !isNullOrUndefined(this.value) ? this.nonNullValue : other();
   }
 
   /**
-   * Return the contained value, if present, otherwise throw an exception
+   * Return the contained value, if present, otherwise throw an error
    * to be created by the provided supplier.
    *
-   * @apiNote A method reference to the exception constructor with an empty
-   * argument list can be used as the supplier. For example,
-   * {@code IllegalStateException::new}
-   *
-   * @param exceptionSupplier The supplier which will return the exception to
-   * be thrown
+   * @param errorSupplier The supplier which will return the error to be thrown
    * @return the present value
-   * @throws X if there is no value present
-   * @throws NullPointerException if no value is present and
-   * {@code exceptionSupplier} is null
+   * @throws Error if there is no value present
+   * @throws Error if no value is present and `errorSupplier` is null
    */
-  public orElseThrow(exceptionSupplier: Producer<Error>): T {
+  public orElseThrow(errorSupplier: Producer<Error>): T {
     if (!isNullOrUndefined(this.value)) {
       return this.nonNullValue;
     } else {
-      throw exceptionSupplier();
+      throw errorSupplier();
     }
   }
 
