b1a76140550332131bfe3acfbc9f1f3f629d1ccc
ILYA Turukin <tr.wolfer@gmail.com>
2020-04-09 18:28:46 +0700

    [#5] Fixed README file. (#17)
    Fixed 'object-utils' TypeDoc.

--- src/object-utils.ts
+++ src/object-utils.ts
@@ -1,6 +1,14 @@
-import { isNotNullOrEmpty, isNullOrEmpty } from './string-utils';
+import {isNotNullOrEmpty, isNullOrEmpty} from './string-utils';
 
-
+/**
+ * Check a passed object against null/undefined.
+ *
+ * @param obj source object to check
+ * @param message error will be thrown with
+ * @return the passed object
+ * @throws <code>Error(message)</code>, or <code>Error('nullable argument')</code>
+ * if no any message has been provided
+ */
 export function requireNonNull<T>(obj: T | null | undefined, message?: string): T {
   if (isNullOrUndefined(obj)) {
     throw new Error(isNotNullOrEmpty(message) ? message : 'nullable argument');
@@ -8,6 +16,15 @@ export function requireNonNull<T>(obj: T | null | undefined, message?: string): 
   return obj as T;
 }
 
+/**
+ * Check a passed string against null/undefined and string length grater than 0.
+ *
+ * @param str source string to check
+ * @param message error will be thrown with
+ * @return the passed string
+ * @throws <code>Error(message)</code>, or <code>Error('nullable or empty argument')</code>
+ * if no any message has been provided
+ */
 export function requireNonEmpty(str: string | null | undefined, message?: string): string {
   if (isNullOrEmpty(str)) {
     throw new Error(isNotNullOrEmpty(message) ? message : 'nullable or empty argument');
